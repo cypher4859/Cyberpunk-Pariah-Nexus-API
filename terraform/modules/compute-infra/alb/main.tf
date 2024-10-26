51ac40568d230d02eac932fff460d1319f168c76
cypher4859 <obsidia_framework_notification@opaquecipher.33mail.com>
2024-10-26 15:27:07 -0400

    Setup the lb listener to listen on port 80

--- terraform/modules/compute-infra/alb/main.tf
+++ terraform/modules/compute-infra/alb/main.tf
@@ -12,7 +12,7 @@ resource "aws_lb" "ecs_alb" {
 
 resource "aws_lb_listener" "ecs_alb_listener" {
     load_balancer_arn = aws_lb.ecs_alb.arn
-    port              = 8080
+    port              = 80
     protocol          = "HTTP"
 
     default_action {
